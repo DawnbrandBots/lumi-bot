297b43a99617ed88c0b39746cb5225f66d3ef0d7
NotABot_FES <notabot.fes@gmail.com>
2026-04-21 22:45:36 +0200

    Add src/search/utils/indexById.ts

---
+++ src/search/utils/indexById.ts
@@ -0,0 +1,12 @@
+export function indexById<T extends { id: string | number | symbol; }>(array: T[]): Record<T["id"], T>;
+export function indexById<T extends { id: { toString(): string; } }>(array: T[]): Record<string, T> {
+    const result: Record<string | number | symbol, T> = {};
+    for (const item of array) {
+        const id = item.id.toString();
+        if (id in result) {
+            throw new Error(`Duplicate id found: ${id}`);
+        }
+        result[id] = item;
+    }
+    return result;
+}
