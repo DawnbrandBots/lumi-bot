164fa26f27739260a25dd91a1b5b219011bfec6f
NotABot_FES <notabot.fes@gmail.com>
2026-04-21 23:02:20 +0200

    Handle search command in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
 import debug from "debug";
 import { ActivityType, Client, Colors, EmbedBuilder, Events, GatewayIntentBits } from "discord.js";
 
+import colorDtos from "../data/colors.json" with { type: "json" };
+import weaponTypeDtos from "../data/weaponTypes.json" with { type: "json" };
+import weaponDtos from "../json/weapon/weapons.json" with { type: "json" };
+
+import initData from "./data/init.js";
+import { searchTermsOptionName, searchCommandName } from "./commands/search.js";
+import { createFuse, search } from "./search/search.js";
+import z from "zod";
+import { color, weapon, weaponType } from "./search/validate.js";
+
+const validatedColorDtos = z.array(color).parse(colorDtos)
+const validatedWeaponTypeDtos = z.array(weaponType).parse(weaponTypeDtos)
+const validatedWeaponDtos = z.array(weapon).parse(weaponDtos)
+
+const { weapons } = initData({
+    colorDtos: validatedColorDtos,
+    weaponTypeDtos: validatedWeaponTypeDtos,
+    weaponDtos: validatedWeaponDtos
+});
+const fuse = createFuse({ items: weapons })
+
 const log = debug("bot");
 
 const bot = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -15,15 +36,33 @@ bot.on(Events.InteractionCreate, async (interaction) => {
     if (!interaction.isChatInputCommand()) {
         return;
     }
-    await interaction.reply({
-        embeds: [
-            new EmbedBuilder()
-                .setColor(Colors.DarkGold)
-                .setTitle("Lumi")
-                .setDescription("Umbra serves the shadow")
-                .setFooter({ text: "Fire Emblem" }),
-        ],
-    });
+    if (interaction.commandName === searchCommandName) {
+        const input = interaction.options.getString(searchTermsOptionName)
+        if (!input) {
+            throw new Error(`No value provided for "${searchTermsOptionName}" option.`)
+        }
+        const result = search({ fuse, search: input });
+        const message = result.success ? `**${result.value.name}** is a level ${result.value.level} ${result.value.type.name}.` : result.msg;
+        await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setColor(Colors.DarkGold)
+                    .setTitle("Lumi")
+                    .setDescription(message)
+                    .setFooter({ text: "Fire Emblem" }),
+            ],
+        });
+    } else {
+        await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setColor(Colors.DarkGold)
+                    .setTitle("Lumi")
+                    .setDescription("Umbra serves the shadow")
+                    .setFooter({ text: "Fire Emblem" }),
+            ],
+        });
+    }
 });
 
 // Implicitly use DISCORD_TOKEN
