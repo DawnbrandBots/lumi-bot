15e3dc7dfbb9f412f9a5e83708d38a1a6b8c980d
NotABot_FES <notabot.fes@gmail.com>
2026-04-21 22:54:15 +0200

    Add searchCommandName in src/commands/search.ts

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -1,11 +1,12 @@
 import { SlashCommandBuilder, ApplicationIntegrationType, InteractionContextType } from "discord.js";
 
+export const searchCommandName = "search";
 export const searchTermsOptionName = "terms";
 
 const searchCommand = new SlashCommandBuilder()
     .setIntegrationTypes(ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall)
     .setContexts(InteractionContextType.Guild, InteractionContextType.BotDM, InteractionContextType.PrivateChannel)
-    .setName("search")
+    .setName(searchCommandName)
     .setDescription("Displays details about weapon, disciple or spell which name resembles search terms the most.")
     .addStringOption(option => option.setName(searchTermsOptionName).setDescription("Name of weapon, disciple or spell to search for.").setRequired(true))
     .toJSON()
