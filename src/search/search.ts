f4d7b0e3ac14571ba0b0ad0ba8fa546350de22cd
NotABot_FES <notabot.fes@gmail.com>
2026-04-21 22:50:34 +0200

    Add src/search/search.ts (weapons only for now)

---
+++ src/search/search.ts
@@ -0,0 +1,26 @@
+import { IWeapon } from "../types.js";
+import Fuse from "fuse.js/basic";
+
+export type SearchIndex = Record<string, IWeapon>;
+export type SearchItem = IWeapon;
+export type SearchResult = { success: true, value: SearchItem } | { success: false, msg: string };
+
+export function createFuse({ items }: { items: (SearchItem)[] }): Fuse<SearchItem> {
+    return new Fuse(items, { keys: ["name"], ignoreDiacritics: true, isCaseSensitive: false });
+}
+
+export function search({ fuse, search }: { fuse: Fuse<SearchItem>, search: string }): SearchResult {
+    const results = fuse.search(search, { limit: 1 });
+    const result = results[0];
+
+    if (!result) {
+        return {
+            success: false,
+            msg: "Search yielded no result.",
+        }
+    }
+    return {
+        success: true,
+        value: result.item
+    }
+}
