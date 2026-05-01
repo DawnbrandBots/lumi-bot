86b04ab5748a8d662f2f766edb149b306a0700c1
NotABot_FES <notabot.fes@gmail.com>
2026-05-01 14:51:53 +0200

    Display disciple stats in response to search command

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -1,7 +1,9 @@
 import { CacheType, ChatInputCommandInteraction, Colors, EmbedBuilder } from "discord.js";
 import Fuse from "fuse.js";
 import { SEARCH_TERMS_OPTION_NAME, searchCommandInfo } from "../commandInfo/search.js";
+import { DISCIPLE_MAX_LEVEL, DISCIPLE_MINIMUM_RELEVANT_LEVEL } from "../constants.ts";
 import { search, SearchItem } from "../search/search.js";
+import { toAsciiTable } from "../utils/table.ts";
 import { Command } from "./base.js";
 
 export function getSearchCommand({ fuse }: { fuse: Fuse<SearchItem> }) {
@@ -19,7 +21,15 @@ export function getSearchCommand({ fuse }: { fuse: Fuse<SearchItem> }) {
                 }
                 const value = result.value
                 if (value.kind === "disciple") {
-                    return `**${value.name}** is a ${value.weaponType.name}-wielding ${value.movementType.name}`
+                    // TODO: adding at least a thumbnail with the character's face would be nice
+                    const columnCountAsideFromHeaderAndLevel1 = DISCIPLE_MAX_LEVEL - DISCIPLE_MINIMUM_RELEVANT_LEVEL + 1
+                    const baseStatsTable = [
+                        ["Level", 1, ...Array.from({ length: columnCountAsideFromHeaderAndLevel1 }, (_, i) => i + DISCIPLE_MINIMUM_RELEVANT_LEVEL)],
+                        ["HP", value.getHp({ level: 1 }), ...Array.from({ length: columnCountAsideFromHeaderAndLevel1 }, (_, i) => value.getHp({ level: i + DISCIPLE_MINIMUM_RELEVANT_LEVEL }))],
+                        ["Atk", value.getAtk({ level: 1 }), ...Array.from({ length: columnCountAsideFromHeaderAndLevel1 }, (_, i) => value.getAtk({ level: i + DISCIPLE_MINIMUM_RELEVANT_LEVEL }))],
+                    ]
+                    const baseStatsTableAscii = toAsciiTable({ data: baseStatsTable, cellPadding: 3 })
+                    return `**${value.name}** is a ${value.weaponType.name}-wielding ${value.movementType.name} disciple.\n\`\`\`\n${baseStatsTableAscii}\n\`\`\``
                 } else if (value.kind === "weapon") {
                     return `**${value.name}** is a level ${value.level} ${value.weaponType.name}.`
                 } else {
