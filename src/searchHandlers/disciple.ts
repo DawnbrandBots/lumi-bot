6f3fb4ca60c9a0979d546fb87420defd53dcdf61
NotABot_FES <notabot.fes@gmail.com>
2026-05-04 17:17:43 +0200

    Display spells in disciple search result in src/searchHandlers/disciple.ts

--- src/searchHandlers/disciple.ts
+++ src/searchHandlers/disciple.ts
@@ -7,6 +7,12 @@ const discipleSearchHandler = {
     response: (disciple: Disciple) => {
         // TODO: adding at least a thumbnail with the character's face would be nice
         // TODO: mention prf weapon
+
+        const introduction = `**${disciple.name}** is a ${disciple.weaponType.name}-wielding ${disciple.movementType.name} disciple.`
+
+        const spellsListStr = disciple.spells.map(spell => `- ${spell.name}`).join(", ")
+        const spellsStr = `**Spells**: ${spellsListStr}`
+
         const columnCountAsideFromHeaderAndLevel1 = DISCIPLE_MAX_LEVEL - DISCIPLE_MINIMUM_RELEVANT_LEVEL + 1
         const baseStatsTable = [
             ["Level", 1, ...Array.from({ length: columnCountAsideFromHeaderAndLevel1 }, (_, i) => i + DISCIPLE_MINIMUM_RELEVANT_LEVEL)],
@@ -14,7 +20,9 @@ const discipleSearchHandler = {
             ["Atk", disciple.getAtk({ level: 1 }), ...Array.from({ length: columnCountAsideFromHeaderAndLevel1 }, (_, i) => disciple.getAtk({ level: i + DISCIPLE_MINIMUM_RELEVANT_LEVEL }))],
         ]
         const baseStatsTableAscii = toAsciiTable({ data: baseStatsTable, cellPadding: 3 })
-        return `**${disciple.name}** is a ${disciple.weaponType.name}-wielding ${disciple.movementType.name} disciple.\n\`\`\`\n${baseStatsTableAscii}\n\`\`\``
+        const baseStatsStr = `**Base stats**\n\`\`\`\n${baseStatsTableAscii}\n\`\`\``
+
+        return [introduction, spellsStr, baseStatsStr].join("\n\n")
     }
 } as const
 
