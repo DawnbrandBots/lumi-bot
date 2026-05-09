f2a94a0d176e71f413632954a8198c631ab23de5
NotABot_FES <notabot.fes@gmail.com>
2026-05-10 01:57:54 +0200

    Move search input max length check from index.ts to src/features/search.ts

--- src/commandInfo/search.ts
+++ src/commandInfo/search.ts
@@ -1,13 +1,11 @@
 import { SlashCommandBuilder } from "discord.js";
+import { SEARCH_MAX_INPUT_LENGTH, SEARCH_TERMS_OPTION_NAME } from "../constants.ts";
 import { CommandInfo, ICommandInfo } from "./base.js";
-
-
-export const SEARCH_TERMS_OPTION_NAME = "terms";
 
 export const searchCommandInfo: ICommandInfo = new CommandInfo({
     customInfo: function (baseInfo: SlashCommandBuilder) {
         return baseInfo
-            .addStringOption(option => option.setName(SEARCH_TERMS_OPTION_NAME).setDescription("Name to search for.").setRequired(true))
+            .addStringOption(option => option.setName(SEARCH_TERMS_OPTION_NAME).setDescription("Name to search for.").setRequired(true).setMaxLength(SEARCH_MAX_INPUT_LENGTH))
     },
     name: "search",
     description: "Displays details about weapon, unique weapon skill, disciple or spell which name resembles search terms the most.",
