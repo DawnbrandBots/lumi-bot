ba9eb3c43686b88adc2dd3a93f0440e386d1acb5
NotABot_FES <notabot.fes@gmail.com>
2026-05-10 12:18:18 +0200

    Shorten searchCommandInfo's description to fit 100 characters limit in src/commandInfo/search.ts

--- src/commandInfo/search.ts
+++ src/commandInfo/search.ts
@@ -8,5 +8,5 @@ export const searchCommandInfo: ICommandInfo = new CommandInfo({
             .addStringOption(option => option.setName(SEARCH_TERMS_OPTION_NAME).setDescription("Name to search for.").setRequired(true).setMaxLength(SEARCH_MAX_INPUT_LENGTH))
     },
     name: "search",
-    description: "Displays details about weapon, unique weapon skill, disciple or spell which name resembles search terms the most.",
+    description: "Displays info about weapon, unique weapon skill, disciple or spell matching search terms the most.",
 })
