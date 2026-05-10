b7fc131b80ef21acb870d9b2affe08085a07451e
NotABot_FES <notabot.fes@gmail.com>
2026-05-10 14:46:15 +0200

    Add scripts to build the database

---
+++ scripts/json-data-to-csv.ts
@@ -0,0 +1,16 @@
+import { stringify } from "csv-stringify/sync";
+
+import fs from 'node:fs';
+
+if (!fs.existsSync("./csv")) {
+    fs.mkdirSync("./csv")
+}
+
+const files = fs.globSync("./data/*.json")
+for (const file of files) {
+    console.info(file)
+    const content = fs.readFileSync(file)
+    const json = JSON.parse(content.toString())
+    const csv = stringify(json)
+    fs.writeFileSync(`./csv/${file.replace(/data\/|\.json/g, "")}.csv`, csv)
+}
