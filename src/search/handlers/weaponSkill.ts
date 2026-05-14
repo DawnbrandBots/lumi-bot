705419d66b74cc926a933a13e637b8ccbcb9fde7
NotABot_FES <notabot.fes@gmail.com>
2026-05-15 00:33:52 +0200

    Remove comment regarding Iterator.join from src/search/handlers/weaponSkill.ts

--- src/search/handlers/weaponSkill.ts
+++ src/search/handlers/weaponSkill.ts
@@ -5,7 +5,6 @@ import type { ISearchHandler } from "../types.ts";
 
 const weaponSkillSearchHandler: ISearchHandler<WeaponSkill> = {
     class: WeaponSkill,
-    // TODO: no join operator on iterators yet :( (https://github.com/tc39/proposal-iterator-join)
     response: (weaponSkill: IWeaponSkill) => {
         const weapons = Array.from(weaponSkill.weapons)
             .map((weapon) => weapon.name)
