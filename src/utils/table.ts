137b6344d3a61b071eb283fd62f143989d8d76f2
NotABot_FES <notabot.fes@gmail.com>
2026-05-14 20:34:44 +0200

    Document src/utils/table.ts

--- src/utils/table.ts
+++ src/utils/table.ts
@@ -1,29 +1,45 @@
-// TODO: should the function be able to compute cellpadding on its own?
-export function toAsciiTable(arg: { data: (string | number)[][]; headerPadding?: number; cellPadding: number }) {
+/**
+ * Returns an ASCII table representation of the given table, using the first row and column as headers.
+ *
+ * Quickly-hacked function, inspired by [ascii-table3](https://github.com/AllMightySauron/ascii-table3),
+ * which is more complete but doesn't provide row headers.
+ */
+export function toAsciiTable(arg: {
+    readonly data: (string | number)[][];
+    /**
+     * Maximum size of cells not on the first column.
+     */
+    readonly cellPadding: number;
+    /**
+     * Maximum size of cells on the first column.
+     */
+    readonly rowHeaderPadding?: number;
+}) {
     if (!arg.data[0]) {
         throw new Error("No first row");
     }
-    const headerPadding = arg.data.reduce((acc, row) => Math.max(acc, row[0]?.toString().length ?? 0), -Infinity) + 1;
+    const rowHeaderPadding =
+        arg.rowHeaderPadding ??
+        arg.data.reduce((acc, row) => Math.max(acc, row[0]?.toString().length ?? 0), -Infinity) + 1;
     function formatRow(row: (string | number)[]) {
-        return rowToStr({ row, headerPadding, cellPadding: arg.cellPadding });
+        return rowToStr({ row, rowHeaderPadding, cellPadding: arg.cellPadding });
     }
     const firstRow = formatRow(arg.data[0]);
-    // const rowSeparator = "─".repeat(5) + " ┼" + " ─── ".repeat(11)
-    const rowSeparator = "─".repeat(headerPadding) + "┼" + "─".repeat(firstRow.length - headerPadding - 1);
+    const rowSeparator = "─".repeat(rowHeaderPadding) + "┼" + "─".repeat(firstRow.length - rowHeaderPadding - 1);
     return [firstRow, rowSeparator, ...arg.data.slice(1).map(formatRow)].join("\n");
 }
 
 function rowToStr({
     row,
-    headerPadding,
+    rowHeaderPadding,
     cellPadding,
 }: {
     row: (string | number)[];
-    headerPadding: number;
+    rowHeaderPadding: number;
     cellPadding: number;
 }) {
     return (
-        row[0]?.toString().padEnd(headerPadding, " ") +
+        row[0]?.toString().padEnd(rowHeaderPadding, " ") +
         "| " +
         row
             .slice(1)
