936aea8fab94c3711579e2c431e94a871f0c9b37
NotABot_FES <notabot.fes@gmail.com>
2026-05-19 17:48:09 +0200

    Add tests for ISearchEngine.search in test/searchEngine.test.ts

--- test/searchEngine.test.ts
+++ test/searchEngine.test.ts
@@ -20,41 +20,58 @@ afterAll(async () => {
 });
 
 describe(FuseSearchEngine.name, () => {
-    // Searching for something, even with upper/lowercase and/or typos, missing vowels, as long as characters are not
-    // too different between input and the thing's name, should return that thing still.
-    test.each(["Royal Sword", "Sword Royal", "royalsword", "ROYAL SWORD"])("%s returns Royal Sword", (input) => {
-        expect(searchEngine.searchOne(input)?.id).toBe("ROYAL_SWORD");
+    describe(FuseSearchEngine.prototype.searchOne.name, () => {
+        // Searching for something, even with upper/lowercase and/or typos, missing vowels, as long as characters are not
+        // too different between input and the thing's name, should return that thing still.
+        test.each(["Royal Sword", "Sword Royal", "royalsword", "ROYAL SWORD"])("%s returns Royal Sword", (input) => {
+            expect(searchEngine.searchOne(input)?.id).toBe("ROYAL_SWORD");
+        });
+
+        // "+" in names could not weight enough to appear first in results,
+        // so the implementation should handle them specially.
+        test.each(["Royal Sword +", "Sword + Royal", "royalsword+", "ROYAL SWORD +", "Royal +", "Royal Sword Plus"])(
+            "%s returns Royal Sword +",
+            (input) => {
+                expect(searchEngine.searchOne(input)?.id).toBe("ROYAL_SWORD_PLUS");
+            },
+        );
+
+        // Spell names can include + as well.
+        // Spells can also be searched by acronym.
+        test.each(["Thunder Shield Break + Cavalry", "TSB+", "TSB+C", "TSBPC", "tsbpc"])(
+            "%s returns Thunder Shield Break + Cavalry",
+            (input) => {
+                expect(searchEngine.searchOne(input)?.id).toBe("THUNDER_SHIELD_BREAK_PLUS_CAVALRY");
+            },
+        );
+
+        // There used to be a bug where weapons with level < 6 would not be retrieved by the DB,
+        // therefore ensure that simply retrieving one weapon per level works.
+        test.each([
+            ["Basic Claws", "BASIC_CLAWS"],
+            ["Iron Axe", "IRON_AXE"],
+            ["Kadomatsu Sword", "KADOMATSU_SWORD"],
+            ["Carved Axe", "CARVED_AXE"],
+            ["Mochi Stone", "MOCHI_STONE"],
+            ["Aerial Lance", "AERIAL_LANCE"],
+            ["Aerial Lance +", "AERIAL_LANCE_PLUS"],
+        ])("finds exact weapon name %s", (input, expectedId) => {
+            expect(searchEngine.searchOne(input)?.id).toBe(expectedId);
+        });
     });
 
-    // "+" in names could not weight enough to appear first in results,
-    // so the implementation should handle them specially.
-    test.each(["Royal Sword +", "Sword + Royal", "royalsword+", "ROYAL SWORD +", "Royal +", "Royal Sword Plus"])(
-        "%s returns Royal Sword +",
-        (input) => {
-            expect(searchEngine.searchOne(input)?.id).toBe("ROYAL_SWORD_PLUS");
-        },
-    );
+    describe(FuseSearchEngine.prototype.search.name, () => {
+        test('results for "Royal" include Royal Sword, Royal Sword + and Royal Scion', () => {
+            const results = searchEngine.search("Royal");
+            const resultIds = results.map((result) => result.id);
+            expect(resultIds).toEqual(expect.arrayContaining(["ROYAL_SWORD", "ROYAL_SWORD_PLUS", "ROYAL_SCION"]));
+        });
 
-    // Spell names can include + as well.
-    // Spells can also be searched by acronym.
-    test.each(["Thunder Shield Break + Cavalry", "TSB+", "TSB+C", "TSBPC", "tsbpc"])(
-        "%s returns Thunder Shield Break + Cavalry",
-        (input) => {
-            expect(searchEngine.searchOne(input)?.id).toBe("THUNDER_SHIELD_BREAK_PLUS_CAVALRY");
-        },
-    );
-
-    // There used to be a bug where weapons with level < 6 would not be retrieved by the DB,
-    // therefore ensure that simply retrieving one weapon per level works.
-    test.each([
-        ["Basic Claws", "BASIC_CLAWS"],
-        ["Iron Axe", "IRON_AXE"],
-        ["Kadomatsu Sword", "KADOMATSU_SWORD"],
-        ["Carved Axe", "CARVED_AXE"],
-        ["Mochi Stone", "MOCHI_STONE"],
-        ["Aerial Lance", "AERIAL_LANCE"],
-        ["Aerial Lance +", "AERIAL_LANCE_PLUS"],
-    ])("finds exact weapon name %s", (input, expectedId) => {
-        expect(searchEngine.searchOne(input)?.id).toBe(expectedId);
+        test(`search limit works`, () => {
+            // Arbitrary number smaller than the total number of weapons with "Sword" in their name.
+            const limit = 3;
+            const results = searchEngine.search("Sword", limit);
+            expect(results.length).toBe(limit);
+        });
     });
 });
