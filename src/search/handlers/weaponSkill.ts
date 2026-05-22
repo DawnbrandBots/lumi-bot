be1da1fb04c8358afbc0e24b4e35eaae4a0109cc
NotABot_FES <notabot.fes@gmail.com>
2026-05-22 09:10:04 +0200

    Display weapon type and weapon level for weapon type skills in src/search/handlers/weaponSkill.ts

--- src/search/handlers/weaponSkill.ts
+++ src/search/handlers/weaponSkill.ts
@@ -1,12 +1,29 @@
 import type { APIEmbed } from "discord.js";
 import { WeaponSkill } from "../../game/models/weaponSkill.ts";
-import type { IWeaponSkill } from "../../game/types.ts";
+import type { IWeaponSkill, IWeaponTypeWeaponSkill } from "../../game/types.ts";
 import type { ISearchHandler } from "../types.ts";
+
+export function getWeaponTypeSkillRankString(rank: IWeaponTypeWeaponSkill["rank"], weaponTypeName: string): string {
+    switch (rank) {
+        case 1:
+            return `Possessed by ${weaponTypeName} weapons of level 3.`;
+        case 2:
+            return `Possessed by ${weaponTypeName} weapons of level 5.`;
+        case 3:
+            return `Possessed by ${weaponTypeName} weapons of level 6, 7 and 8.`;
+    }
+}
 
 const weaponSkillSearchHandler: ISearchHandler<WeaponSkill> = {
     class: WeaponSkill,
     response: (weaponSkill: IWeaponSkill) => {
-        const weapons = Array.from(weaponSkill.uniqueSkillWeapons)
+        const weaponTypeSkillStr = Array.from(weaponSkill.weaponTypeWeaponSkills)
+            .map((weaponTypeWeaponSkill) =>
+                getWeaponTypeSkillRankString(weaponTypeWeaponSkill.rank, weaponTypeWeaponSkill.weaponType.name),
+            )
+            .join("\n");
+
+        const uniqueSkillWeapons = Array.from(weaponSkill.uniqueSkillWeapons)
             .map((weapon) => weapon.name)
             .join(", ");
 
@@ -16,11 +33,19 @@ const weaponSkillSearchHandler: ISearchHandler<WeaponSkill> = {
                 value: weaponSkill.description,
                 inline: true,
             },
-            ...(weapons.length > 0
+            ...(weaponTypeSkillStr.length > 0
+                ? [
+                    {
+                        name: "Weapon Type Skill",
+                        value: weaponTypeSkillStr,
+                    },
+                ]
+                : []),
+            ...(uniqueSkillWeapons.length > 0
                 ? [
                     {
                         name: "Unique skill for weapon(s)",
-                        value: weapons,
+                        value: uniqueSkillWeapons,
                     },
                 ]
                 : []),
