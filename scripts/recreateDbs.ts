b65d61934f4fd34561db37d9670d2bcb5ff4012b
NotABot_FES <notabot.fes@gmail.com>
2026-05-23 17:58:25 +0200

    Remove file comment from scripts/recreateDbs.ts

--- scripts/recreateDbs.ts
+++ scripts/recreateDbs.ts
@@ -1,9 +1,3 @@
-/**
- * @file Creates an SQLite database with game data.
- *
- * Takes the path to mikro-orm.config.ts as argument.
- */
-
 import { configsById } from "../src/mikro-orm.config.ts";
 import recreateGameDb from "./utils/recreateGameDb.ts";
 
