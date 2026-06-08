2c690c7d44e2c153608545a86f39539bde973d0f
NotABot_FES <notabot.fes@gmail.com>
2026-06-08 19:32:21 +0200

    Update scripts/updateBotProfile.ts by calling DiscordJS' REST handler

--- scripts/updateBotProfile.ts
+++ scripts/updateBotProfile.ts
@@ -1,3 +1,4 @@
+import { REST, Routes } from "discord.js";
 import { DISCORD_BOT_ABOUT_ME } from "../src/bot/constants.ts";
 
 const tokenKey = "DISCORD_TOKEN";
@@ -6,11 +7,9 @@ if (!token) {
     throw new Error(`"${tokenKey}" environment variable required`);
 }
 
-const response = await fetch("https://discord.com/api/applications/@me", {
-    method: "PATCH",
-    headers: { Authorization: `Bot ${token}`, "Content-Type": "application/json" },
-    body: JSON.stringify({ description: DISCORD_BOT_ABOUT_ME }),
+const api = new REST().setToken(token);
+const body = await api.patch(Routes.currentApplication(), {
+    body: { description: DISCORD_BOT_ABOUT_ME },
 });
-const body = await response.text();
 
 console.log(body);
