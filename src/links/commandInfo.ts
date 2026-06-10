eb3456bce127e50c411e1a5c5c1723ead6bff8f1
NotABot_FES <notabot.fes@gmail.com>
2026-06-10 20:49:20 +0200

    Delete src/links/constants.ts

--- src/links/commandInfo.ts
+++ src/links/commandInfo.ts
@@ -1,9 +1,8 @@
 import { CommandInfo } from "../bot/commandInfo.ts";
 import type { ICommandInfo } from "../bot/types.ts";
-import { LINKS_COMMAND_NAME } from "./constants.ts";
 
 export const linksCommandInfo: ICommandInfo = new CommandInfo({
     customInfo: (baseInfo) => baseInfo,
-    name: LINKS_COMMAND_NAME,
+    name: "links",
     description: "Displays official Fire Emblem Shadows links.",
 });
