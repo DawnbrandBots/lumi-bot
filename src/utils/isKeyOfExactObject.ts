39393a31226434566334a0248f746a58cf5dbb27
NotABot_FES <notabot.fes@gmail.com>
2026-06-10 20:13:37 +0200

    Add link to TypeScript repo issue regarding Exact types in src/utils/isKeyOfExactObject.ts

--- src/utils/isKeyOfExactObject.ts
+++ src/utils/isKeyOfExactObject.ts
@@ -1,10 +1,11 @@
 /**
  * Type guard to only ever use with constant objects which type describe exactly the keys present in the objects.
+ *
+ * Hopefully this is implemented one day: https://github.com/microsoft/TypeScript/issues/12936.
  */
 export default function isKeyOfExactObject<O extends Record<string, unknown>>(
     obj: O,
     key: PropertyKey,
 ): key is keyof O {
-    // ): key is K {
     return key in obj;
 }
