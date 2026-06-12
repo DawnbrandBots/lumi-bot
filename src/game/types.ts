4acdb38ca2224ec7343a010608f60fc3522c7778
NotABot_FES <notabot.fes@gmail.com>
2026-06-12 18:15:35 +0200

    Add scalesWithLevel property to spell effect value types and classes

--- src/game/types.ts
+++ src/game/types.ts
@@ -340,6 +340,7 @@ export interface ISpellEffectValue {
      * Value of spell effect for the spell's level 1.
      */
     readonly base: number;
+    readonly scalesWithLevel?: boolean;
     readonly unit: ISpellEffectValueUnit;
     readonly effectiveness?: ISpellEffectValueEffectivenessItem[] | null;
 }
@@ -398,6 +399,7 @@ export interface ISpellEffectTarget {
 // TODO: scale property added in later PR
 export interface ISummonEffectStatValue {
     readonly base: number;
+    readonly scalesWithLevel?: boolean;
 }
 
 /**
