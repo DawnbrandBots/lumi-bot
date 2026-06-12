12c2a4bbf99aa34047a1e97e363cdef827c80cde
NotABot_FES <notabot.fes@gmail.com>
2026-06-13 00:05:35 +0200

    Rename "config" to "guild_config" in Migration file

--- src/migrations/lumi/Migration20260526141000.ts
+++ src/migrations/lumi/Migration20260526141000.ts
@@ -3,8 +3,8 @@ import { Migration } from "@mikro-orm/migrations";
 export class Migration20260526141000 extends Migration {
     override up(): void {
         this.addSql(
-            "create table `config` (`id` text not null primary key, `guild` text not null, `channel` text null default null);",
+            "create table `guild_config` (`id` text not null primary key, `guild` text not null, `channel` text null default null);",
         );
-        this.addSql("create unique index `config_guild_unique` on `config` (`guild`);");
+        this.addSql("create unique index `guild_config_guild_unique` on `guild_config` (`guild`);");
     }
 }
