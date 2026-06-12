8ab2a707a1aa49346c23c557a2f7380b505ff28e
NotABot_FES <notabot.fes@gmail.com>
2026-06-12 19:19:33 +0200

    Do not expose commands already in production

--- src/loaders/commandInfo.ts
+++ src/loaders/commandInfo.ts
@@ -1,8 +1,12 @@
 import { adminCommandInfo } from "../admin/commandInfo.ts";
 import { helpCommandInfo } from "../help/commandInfo.ts";
 import { lfgCommandInfo } from "../lfg/commandInfo.ts";
-import { linksCommandInfo } from "../links/commandInfo.ts";
-import { searchCommandInfo } from "../search/commandInfo.ts";
 
-const allCommandInfo = [helpCommandInfo, searchCommandInfo, linksCommandInfo, lfgCommandInfo, adminCommandInfo];
+const allCommandInfo = [
+    helpCommandInfo,
+    // searchCommandInfo,
+    // linksCommandInfo,
+    lfgCommandInfo,
+    adminCommandInfo,
+];
 export default allCommandInfo;
