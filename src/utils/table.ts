703e33cd223bdc07d39c4e469448ba8b8e3a44db
NotABot_FES <notabot.fes@gmail.com>
2026-06-14 19:47:15 +0200

    Add headerRowsCount property to toAsciiTable's arg

--- src/utils/table.ts
+++ src/utils/table.ts
@@ -14,9 +14,14 @@ export function toAsciiTable(arg: {
      * Maximum size of cells on the first column.
      */
     readonly rowHeaderPadding?: number;
+    /**
+     * Number of rows part of table's header.
+     */
+    readonly headerRowsCount?: number;
 }) {
-    if (!arg.data[0]) {
-        throw new Error("No first row");
+    const headerRowsCount = arg.headerRowsCount ?? 1;
+    if (headerRowsCount > arg.data.length) {
+        throw new Error("Number of header rows greater than number of rows");
     }
     const rowHeaderPadding =
         arg.rowHeaderPadding ??
@@ -24,9 +29,13 @@ export function toAsciiTable(arg: {
     function formatRow(row: (string | number)[]) {
         return rowToStr({ row, rowHeaderPadding, cellPadding: arg.cellPadding });
     }
-    const firstRow = formatRow(arg.data[0]);
-    const rowSeparator = "─".repeat(rowHeaderPadding) + "┼" + "─".repeat(firstRow.length - rowHeaderPadding - 1);
-    return [firstRow, rowSeparator, ...arg.data.slice(1).map(formatRow)].join("\n");
+    const headerRows = arg.data.slice(0, headerRowsCount).map(formatRow);
+    const rowSeparatorSecondHalfLength =
+        headerRows.reduce((acc, row) => Math.max(acc, row.length), headerRows[0]!.length - rowHeaderPadding - 1) -
+        rowHeaderPadding -
+        1;
+    const rowSeparator = "─".repeat(rowHeaderPadding) + "┼" + "─".repeat(rowSeparatorSecondHalfLength);
+    return [...headerRows, rowSeparator, ...arg.data.slice(headerRowsCount).map(formatRow)].join("\n");
 }
 
 function rowToStr({
@@ -44,6 +53,6 @@ function rowToStr({
         row
             .slice(1)
             .map((n) => `${n}`.padStart(cellPadding, " "))
-            .join("  ")
+            .join(" ")
     );
 }
