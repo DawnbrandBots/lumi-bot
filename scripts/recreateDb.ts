179c7a4d935a6a7318b5071e8ebc7c06c1312fda
NotABot_FES <notabot.fes@gmail.com>
2026-06-14 15:58:36 +0200

    Remove unused scripts/recreateDb.ts

--- scripts/recreateDb.ts
+++
@@ -1,8 +0,0 @@
-import { appMikroOrmConfig, migrationMikroOrmConfig, staticGameDataMikroOrmConfig } from "../src/mikro-orm.config.ts";
-import recreateDb from "./utils/recreateDb.ts";
-
-await recreateDb({
-    appConfig: appMikroOrmConfig,
-    migrationConfig: migrationMikroOrmConfig,
-    staticGameDataConfig: staticGameDataMikroOrmConfig,
-});
