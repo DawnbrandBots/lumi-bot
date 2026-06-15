335d5202290d38f83ed48bf41b71d129654b7a91
NotABot_FES <notabot.fes@gmail.com>
2026-06-15 16:16:52 +0200

    Remove outdates changes to help command

--- src/help/commandInfo.ts
+++ src/help/commandInfo.ts
@@ -1,23 +1,11 @@
 import { CommandInfo } from "../bot/commandInfo.ts";
 import { DISCORD_BOT_NAME } from "../bot/constants.ts";
 import type { ICommandInfo } from "../bot/types.ts";
-import { HELP_BOT_SUBCOMMAND_NAME, HELP_SHADOWS_SUBCOMMAND_NAME } from "./constants.ts";
 
 export const helpCommandInfo: ICommandInfo = new CommandInfo({
     customInfo: function (baseInfo) {
-        return baseInfo
-            .addSubcommand((subcommand) =>
-                subcommand
-                    .setName(HELP_BOT_SUBCOMMAND_NAME)
-                    .setDescription(`Displays help for ${DISCORD_BOT_NAME} bot.`),
-            )
-            .addSubcommand((subcommand) =>
-                subcommand
-                    .setName(HELP_SHADOWS_SUBCOMMAND_NAME)
-                    .setDescription("Displays official Fire Emblem Shadows links."),
-            );
+        return baseInfo;
     },
     name: "help",
     description: `Displays help for ${DISCORD_BOT_NAME} bot.`,
-    pingEquivalent: `@${DISCORD_BOT_NAME}`,
 });
