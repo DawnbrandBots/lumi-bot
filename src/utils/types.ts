06407aebceaa3a3c8ec82569ad652683af472495
NotABot_FES <notabot.fes@gmail.com>
2026-06-26 23:56:58 +0200

    Move Subset as DeepPick from src/game/discipleStats.ts to src/utils/types.ts

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,1 +1,17 @@
 export type MaybePromise<T> = T | PromiseLike<T>;
+
+type DeepPickShape<T> = {
+    [TKey in keyof T]?: T[TKey] extends object ? true | DeepPickShape<T[TKey]> : true;
+};
+
+/**
+ * Recursively selects properties from a type while deriving leaf types from it.
+ * Use `true` to select a leaf property.
+ */
+export type DeepPick<T, TShape extends DeepPickShape<T>> = {
+    [TKey in keyof TShape & keyof T]: NonNullable<TShape[TKey]> extends true
+        ? T[TKey]
+        : NonNullable<TShape[TKey]> extends DeepPickShape<T[TKey]>
+          ? DeepPick<T[TKey], NonNullable<TShape[TKey]>>
+          : never;
+};
