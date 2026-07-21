d2ceeb033a511b83641838d7d361da95a6bb8763
NotABot-FES <notabot.fes@gmail.com>
2026-07-21 22:58:50 +0200

    Additional search aliases (#102)
    * Make alias functions receive the whole entity instead of just its name in src/loaders/searchItems.ts
    
    * Comment out tests "single alias => footer absent"
    
    * Add new search aliases
    
    * Return empty array from FuseSearchEngine.search when empty string is given
    
    * Update aliases
    
    * Add fastest-levenshtein
    
    * Update SEARCH_RANKING_CASES in test/search/constants.ts
    
    * Test empty array when search input is empty
    
    * Move some failing test inputs from SEARCH_RANKING_CASES to SEARCH_RANKING_KNOWN_FAILURE_CASES
    
    * Also add "SPELL NAME's disciple" as alias for spells
    
    * Test new aliases
    
    * Rename getAliasLengthDistance to getAliasDistanceToInput
    
    * Remove commented out code from src/search/engine.ts
    
    * Rename normalize to removeDiacritics and move it to src/utils/removeDiacritics.ts
    
    * Add comments in src/search/engine.ts
    
    * Comment src/utils/removeDiacritics.ts
    
    * Split search alias generators
    
    * Use standalone aliases in relative aliases
    
    * Remove all occurrences of "'s" from src/loaders/searchItems.ts
    
    * Test related standalone search aliases
    
    * Merge related search ranking cases
    
    * Comment on standalone and relative aliases in src/loaders/searchItems.ts

--- src/loaders/searchItems.ts
+++ src/loaders/searchItems.ts
@@ -5,31 +5,129 @@ import { Music } from "../game/models/music.ts";
 import { Spell } from "../game/models/spell.ts";
 import { Weapon } from "../game/models/weapon.ts";
 import { WeaponSkill } from "../game/models/weaponSkill.ts";
-import type { IMusic } from "../game/types.ts";
-import type { ISearchableEntity, ISearchItem } from "../search/types.ts";
+import { ESpellRole } from "../game/types.ts";
+import type { ISearchItem, TSearchableEntity } from "../search/types.ts";
 
-function* id(value: string) {
-    yield value;
+// Standalone aliases are aliases created from an entity's own properties. eg. `Ennea Fire EX` and `EFEX` are based on the spell's name only.
+// Relative aliases are aliases created from an entity's relationship's properties. eg. `Ennea Fire EX disciple` points to `Kurt`,
+// and so do `EFEX Disciple`, `Royal Sword + disciple` and `Royal Sword Plus disciple`!
+
+function* standaloneAliasWeapon(weapon: Weapon) {
+    yield weapon.name;
+    if (weapon.name.includes("+")) {
+        yield weapon.name.replace("+", "Plus");
+    }
 }
 
-function* aliasWeaponName(value: string) {
-    yield value.replace("+", "Plus");
+function* relativeAliasWeapon(weapon: Weapon) {
+    if (weapon.prfDisciple) {
+        for (const discipleAlias of standaloneAliasDisciple(weapon.prfDisciple)) {
+            yield `${discipleAlias} weapon`;
+        }
+    }
+}
+
+function* aliasWeapon(weapon: Weapon) {
+    yield* standaloneAliasWeapon(weapon);
+    yield* relativeAliasWeapon(weapon);
+}
+
+function* standaloneAliasDisciple(disciple: Disciple) {
+    yield disciple.name;
+}
+
+function* relativeAliasDisciple(disciple: Disciple) {
+    if (disciple.prfWeapon) {
+        for (const weaponAlias of standaloneAliasWeapon(disciple.prfWeapon)) {
+            yield `${weaponAlias} disciple`;
+        }
+    }
+    for (const spell of disciple.spells) {
+        for (const spellAlias of standaloneAliasSpell(spell)) {
+            yield `${spellAlias} disciple`;
+        }
+    }
+}
+
+function* aliasDisciple(disciple: Disciple) {
+    yield* standaloneAliasDisciple(disciple);
+    yield* relativeAliasDisciple(disciple);
+}
+
+function* standaloneAliasWeaponSkill(weaponSkill: WeaponSkill) {
+    yield weaponSkill.name;
+}
+
+function* relativeAliasWeaponSkill(weaponSkill: WeaponSkill) {
+    for (const weapon of weaponSkill.uniqueSkillWeapons) {
+        for (const weaponAlias of standaloneAliasWeapon(weapon)) {
+            yield `${weaponAlias} weapon skill`;
+        }
+    }
+}
+
+function* aliasWeaponSkill(weaponSkill: WeaponSkill) {
+    yield* standaloneAliasWeaponSkill(weaponSkill);
+    yield* relativeAliasWeaponSkill(weaponSkill);
+}
+
+function* standaloneAliasMusic(music: Music) {
+    yield music.name;
+}
+
+function* relativeAliasMusic(music: Music) {
+    for (const disciple of music.shadowMusicFor || []) {
+        for (const discipleAlias of standaloneAliasDisciple(disciple)) {
+            yield `Shadow ${discipleAlias} music`;
+        }
+    }
+    for (const disciple of music.shadowResultsScreenMusicFor || []) {
+        for (const discipleAlias of standaloneAliasDisciple(disciple)) {
+            yield `Shadow ${discipleAlias} results screen music`;
+        }
+    }
+}
+
+function* aliasMusic(music: Music) {
+    yield* standaloneAliasMusic(music);
+    yield* relativeAliasMusic(music);
 }
 
 const SPELL_NAME_PREFIX_SPLIT_REGEX = new RegExp(`\\s|(?=${SPELL_NAME_SUFFIXES.join("|")})`, "i");
 
-function* aliasSpellName(value: string): Generator<string> {
-    const norm = value.replace("+", "Plus");
-    yield norm;
-    const normSplit = norm.split(SPELL_NAME_PREFIX_SPLIT_REGEX);
-    yield normSplit.map((s) => (s === "EX" ? s : s === "Plus" ? "P" : s[0]?.toUpperCase())).join("");
+function* standaloneAliasSpell(spell: Spell): Generator<string> {
+    yield spell.name;
+    if (spell.name.includes("+")) {
+        yield spell.name.replace("+", "Plus");
+    }
+
+    const nameSplit = spell.name.split(SPELL_NAME_PREFIX_SPLIT_REGEX);
+    const acronym = nameSplit.map((s) => (s === "EX" ? s : s[0]?.toUpperCase())).join("");
+    yield acronym;
+    if (spell.name.includes("+")) {
+        yield acronym.replace("+", "P");
+    }
+}
+
+function* relativeAliasSpell(spell: Spell): Generator<string> {
+    if (spell.disciple && spell.role.kind === ESpellRole.EX) {
+        for (const discipleAlias of standaloneAliasDisciple(spell.disciple)) {
+            yield `${discipleAlias} EX`;
+        }
+    }
+}
+
+function* aliasSpell(spell: Spell): Generator<string> {
+    yield* standaloneAliasSpell(spell);
+    yield* relativeAliasSpell(spell);
 }
 
 function getToSearchItemMapper<Kind extends string>(
-    aliaser: (value: string) => Iterable<string>,
-): (entity: ISearchableEntity & { kind: Kind }) => ISearchItem & { kind: Kind } {
-    return ({ id, name, kind }: ISearchableEntity & { kind: Kind }) => {
-        const aliases = [...aliaser(name)];
+    aliaser: (value: Extract<TSearchableEntity, { kind: Kind }>) => Iterable<string>,
+): (entity: Extract<TSearchableEntity, { kind: Kind }>) => ISearchItem & { kind: Kind } {
+    return (entity: Extract<TSearchableEntity, { kind: Kind }>) => {
+        const aliases = [...aliaser(entity)];
+        const { id, name, kind } = entity;
         return {
             id,
             name,
@@ -47,17 +145,19 @@ export default async function getSearchItems(em: SqlEntityManager) {
     const localEm = em.fork();
 
     // No need to populate entities. We only care about the id, name and kind for the sake of the search.
-    const weapons: Weapon[] = await localEm.findAll(Weapon);
-    const disciples: Disciple[] = await localEm.findAll(Disciple);
-    const weaponSkills: WeaponSkill[] = await localEm.findAll(WeaponSkill);
-    const spells: Spell[] = await localEm.findAll(Spell);
-    const music: IMusic[] = await localEm.findAll(Music);
+    const weapons: Weapon[] = await localEm.findAll(Weapon, { populate: ["prfDisciple"] });
+    const disciples: Disciple[] = await localEm.findAll(Disciple, { populate: ["prfWeapon", "spells"] });
+    const weaponSkills: WeaponSkill[] = await localEm.findAll(WeaponSkill, { populate: ["uniqueSkillWeapons"] });
+    const spells: Spell[] = await localEm.findAll(Spell, { populate: ["disciple"] });
+    const music: Music[] = await localEm.findAll(Music, {
+        populate: ["shadowMusicFor", "shadowResultsScreenMusicFor"],
+    });
 
-    const weaponSearchItems = weapons.flatMap(getToSearchItemMapper(aliasWeaponName));
-    const discipleSearchItems = disciples.flatMap(getToSearchItemMapper(id));
-    const weaponSkillSearchItems = weaponSkills.flatMap(getToSearchItemMapper(id));
-    const spellSearchItems = spells.flatMap(getToSearchItemMapper(aliasSpellName));
-    const musicSearchItems = music.flatMap(getToSearchItemMapper(id));
+    const weaponSearchItems = weapons.flatMap(getToSearchItemMapper(aliasWeapon));
+    const discipleSearchItems = disciples.flatMap(getToSearchItemMapper(aliasDisciple));
+    const weaponSkillSearchItems = weaponSkills.flatMap(getToSearchItemMapper(aliasWeaponSkill));
+    const spellSearchItems = spells.flatMap(getToSearchItemMapper(aliasSpell));
+    const musicSearchItems = music.flatMap(getToSearchItemMapper(aliasMusic));
 
     return [
         ...weaponSearchItems,
--- src/search/engine.ts
+++ src/search/engine.ts
@@ -1,5 +1,30 @@
-import Fuse from "fuse.js";
+import { distance } from "fastest-levenshtein";
+import Fuse, { type FuseSortFunctionArg } from "fuse.js";
+import removeDiacritics from "../utils/removeDiacritics.ts";
 import type { ISearchEngine, ISearchItem } from "./types.ts";
+
+/** Alias match type provided by Fuse to custom sort functions. */
+type TAliasMatch = NonNullable<FuseSortFunctionArg["matches"]>[number];
+
+/**
+ * Returns the alias with the best score in the given ${@link FuseSortFunctionArg}.
+ *
+ * As an example, the fuse item for the "Ennea Fire EX" spell can have the "Ennea Fire EX", "EFEX" and "Kurt's EX" aliases.
+ * Given "Kurt" as input, "Kurt's EX" is the alias that's the closest, and therefore its score is the best.
+ * The fuse match for "Kurt's EX" in the "Ennea Fire EX" fuse item will be returned.
+ */
+function getBestAlias(result: FuseSortFunctionArg): TAliasMatch | null {
+    return (
+        result.matches?.reduce<TAliasMatch | null>(
+            (bestAlias, alias) => (bestAlias && bestAlias.score <= alias.score ? bestAlias : alias),
+            null,
+        ) ?? null
+    );
+}
+
+function getAliasDistanceToInput({ alias, input }: { alias: TAliasMatch | null; input: string }): number {
+    return alias ? distance(removeDiacritics(alias.value), removeDiacritics(input)) : Number.POSITIVE_INFINITY;
+}
 
 export abstract class SearchEngine<Items extends ISearchItem> implements ISearchEngine<Items> {
     abstract searchOne(input: string): Items | undefined;
@@ -8,6 +33,8 @@ export abstract class SearchEngine<Items extends ISearchItem> implements ISearch
 
 export class FuseSearchEngine<Items extends ISearchItem> extends SearchEngine<Items> {
     private readonly fuse: Fuse<Items>;
+    /** Used in the custom fuse sorting function. `sortFn` does not receive the search input as argument. */
+    private lastInput: string = "";
 
     public constructor({ items }: { items: Items[] }) {
         super();
@@ -18,15 +45,43 @@ export class FuseSearchEngine<Items extends ISearchItem> extends SearchEngine<It
             isCaseSensitive: false,
             useTokenSearch: true,
             ignoreLocation: true,
+            sortFn: (a, b) => this.sortByBestAliasScore(a, b),
         });
     }
 
-    protected normalizeInput(input: string): string {
-        return input.replace("+", "Plus");
+    /**
+     * By default, fuse scores items and sorts them by their score, best first.
+     * When items have multiple aliases, an item's score appears to be the result of a certain operation on all of its aliases' score (most likely multiplication).
+     * This may cause unwanted results to receive a better score.
+     *
+     * For example, if the "Royal Sword +" fuse item has aliases "Royal Sword +", "Royal Sword Plus" and "Kurt's Weapon",
+     * and the "Royal Scion" fuse item has aliases "Royal Scion", "Royal Sword +'s weapon skill" and "Royal Sword + weapon skill",
+     * If given "Royal Sword" as input, with the default sorting, "Royal Scion" will appear first:
+     * "Royal Sword" matches two aliases in each item, but "Royal" also partially matches "Royal Scion", but not "Kurt's Weapon".
+     *
+     * This sorting method instead uses each item's best matching alias' score for comparison.
+     * In case of equality, then the item's levenshtein distance relative to the input is used for comparison.
+     *
+     * Fuse gives the same score of 0.01 to "Royal Sword's skill" and "Royal Sword" when given "Royal Sword" as input.
+     * The Levenshtein distance breaks the tie by prioritizing the item which is actually closest to the input.
+     */
+    protected sortByBestAliasScore(a: FuseSortFunctionArg, b: FuseSortFunctionArg): number {
+        const aBestAlias = getBestAlias(a);
+        const bBestAlias = getBestAlias(b);
+        return (
+            (aBestAlias?.score ?? 1) - (bBestAlias?.score ?? 1) ||
+            getAliasDistanceToInput({ alias: aBestAlias, input: this.lastInput }) -
+                getAliasDistanceToInput({ alias: bBestAlias, input: this.lastInput }) ||
+            a.idx - b.idx
+        );
     }
 
     public search(input: string, limit?: number): Items[] {
-        return this.fuse.search(this.normalizeInput(input), { limit }).map((result) => result.item);
+        if (input.length === 0) {
+            return [];
+        }
+        this.lastInput = input;
+        return this.fuse.search(this.lastInput, { limit }).map((result) => result.item);
     }
 
     public searchOne(input: string): Items | undefined {
