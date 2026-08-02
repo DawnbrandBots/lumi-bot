b097b321b5922263e8aa26c985137a0517ea5c23
NotABot_FES <notabot.fes@gmail.com>
2026-08-02 16:34:17 +0200

    Remove mkdir from scripts/dockerInitDbs.sh

--- scripts/dockerInitDbs.sh
+++ scripts/dockerInitDbs.sh
@@ -6,7 +6,7 @@ if [ -z "${LUMI_STATE_DB_DIR:-}" ] || [ -z "${LUMI_STATIC_DB_DIR:-}" ]; then
     exit 1
 fi
 
-mkdir -p "$LUMI_STATE_DB_DIR" "$LUMI_STATIC_DB_DIR"
+# Need to change ownership of volumes directories from root to container user (node)
+# to not cause errors when writing to these directories.
 chown -R node:node "$LUMI_STATE_DB_DIR" "$LUMI_STATIC_DB_DIR"
-
 runuser -u node -- sh -c 'yarn db:recreate && yarn db:migrate'
