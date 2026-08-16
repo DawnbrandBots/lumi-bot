0a52f9d3232bdd238375c38f5a8b7c3d8f5ff9e6
NotABot_FES <notabot.fes@gmail.com>
2026-08-16 21:59:05 +0200

    Move smaller variables definitions into the bigger variable definition in test/unit/search/mappers/spell.fixtures.ts

--- test/unit/search/mappers/spell.fixtures.ts
+++ test/unit/search/mappers/spell.fixtures.ts
@@ -6,28 +6,16 @@ import {
     ESpellEffectTarget,
     ESpellEffectValueUnitKind,
     ESpellRole,
-    type ISpell,
-    type ISpellShape,
+    type ISpell
 } from "../../../../src/game/types.ts";
 import { RED_COLOR } from "./common.fixtures.ts";
-
-export const SPELL_ROLE = ESpellRole.EX satisfies ISpell["role"];
-
-export const SPELL_SHAPE = {
-    id: "SINGLE_TILE",
-    name: "single space",
-    tiles: "............X............",
-    isAoe: false,
-} satisfies ISpellShape;
-
-export const SPELL_DRAGGING_MODE = ESpellDraggingMode.ANY satisfies ISpell["draggingMode"];
 
 export const SPELL = {
     kind: "spell",
     id: "ELFIRE",
     name: "Elfire",
     disciple: null,
-    role: SPELL_ROLE,
+    role: ESpellRole.EX,
     uses: null,
     countdown: null,
     cooldown: 5,
@@ -44,7 +32,12 @@ export const SPELL = {
             target: ESpellEffectTarget.ANY,
         },
     ],
-    shape: SPELL_SHAPE,
+    shape: {
+        id: "SINGLE_TILE",
+        name: "single space",
+        tiles: "............X............",
+        isAoe: false,
+    },
     onlyFor: null,
-    draggingMode: SPELL_DRAGGING_MODE,
+    draggingMode: ESpellDraggingMode.ANY,
 } satisfies ISpell;
