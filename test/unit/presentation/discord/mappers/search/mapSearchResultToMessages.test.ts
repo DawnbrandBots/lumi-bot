f37c3bbd08a80d8ed447ebbe743b8a52833b708c
NotABot_FES <notabot.fes@gmail.com>
2026-08-18 23:23:16 +0200

    Snapshot search result mapper tests

--- test/unit/presentation/discord/mappers/search/mapSearchResultToMessages.test.ts
+++ test/unit/presentation/discord/mappers/search/mapSearchResultToMessages.test.ts
@@ -1,16 +1,7 @@
 import { describe, expect, test } from "vitest";
-import { SEARCH_MAX_INPUT_LENGTH } from "../../../../../../src/application/search/constants.ts";
 import { ESearchResultKind } from "../../../../../../src/application/search/types.ts";
-import { DISCORD_ERROR_MESSAGE_DEFAULT_CONTENT } from "../../../../../../src/presentation/discord/constants.ts";
 import mapSearchResultToMessages from "../../../../../../src/presentation/discord/mappers/search.ts";
-import { EMessageKind } from "../../../../../../src/presentation/discord/message.types.ts";
-import { MUSIC, MUSIC_SEARCH_SUCCESS_VALUE } from "./root.fixtures.ts";
-
-const SEARCH_ENTITY_KIND_FIELD_NAME = "Entity kind";
-const SEARCH_ID_FIELD_NAME = "Id";
-const SEARCH_INPUT_TOO_LONG_DESCRIPTION = `Input too long. Maximum is ${SEARCH_MAX_INPUT_LENGTH} characters.`;
-const SEARCH_YIELDED_NO_RESULT_DESCRIPTION = "Search yielded no result";
-const SEARCH_MISSING_DATABASE_RESULT_DESCRIPTION = "Result found in search engine but not in database";
+import { MUSIC_SEARCH_SUCCESS_VALUE } from "./root.fixtures.ts";
 
 describe(mapSearchResultToMessages.name, () => {
     test.each([
@@ -20,47 +11,17 @@ describe(mapSearchResultToMessages.name, () => {
                 kind: ESearchResultKind.SUCCESS,
                 value: MUSIC_SEARCH_SUCCESS_VALUE,
             },
-            {
-                reply: {
-                    kind: EMessageKind.POSITIVE,
-                    embeds: [
-                        {
-                            title: MUSIC.name,
-                        },
-                    ],
-                },
-            },
         ],
         [
             ESearchResultKind.INPUT_TOO_LONG,
             {
                 kind: ESearchResultKind.INPUT_TOO_LONG,
             },
-            {
-                reply: {
-                    kind: EMessageKind.NEGATIVE,
-                    embeds: [
-                        {
-                            description: SEARCH_INPUT_TOO_LONG_DESCRIPTION,
-                        },
-                    ],
-                },
-            },
         ],
         [
             ESearchResultKind.NO_RESULT,
             {
                 kind: ESearchResultKind.NO_RESULT,
-            },
-            {
-                reply: {
-                    kind: EMessageKind.NEGATIVE,
-                    embeds: [
-                        {
-                            description: SEARCH_YIELDED_NO_RESULT_DESCRIPTION,
-                        },
-                    ],
-                },
             },
         ],
         [
@@ -72,23 +33,8 @@ describe(mapSearchResultToMessages.name, () => {
                     id: "MISSING_WEAPON",
                 },
             },
-            {
-                reply: {
-                    kind: EMessageKind.ERROR,
-                    content: DISCORD_ERROR_MESSAGE_DEFAULT_CONTENT,
-                    embeds: [
-                        {
-                            description: SEARCH_MISSING_DATABASE_RESULT_DESCRIPTION,
-                            fields: [
-                                { name: SEARCH_ENTITY_KIND_FIELD_NAME, value: "weapon", inline: true },
-                                { name: SEARCH_ID_FIELD_NAME, value: "MISSING_WEAPON", inline: true },
-                            ],
-                        },
-                    ],
-                },
-            },
         ],
-    ] as const)("%s", (_, result, expected) => {
-        expect(mapSearchResultToMessages(result)).toMatchObject(expected);
+    ] as const)("%s", (_, result) => {
+        expect(mapSearchResultToMessages(result)).toMatchSnapshot();
     });
 });
