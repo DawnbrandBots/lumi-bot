6eb82b31ed9030e13563f1e23c3665a33b84f1e2
NotABot_FES <notabot.fes@gmail.com>
2026-08-19 11:57:52 +0200

    Snapshot lfg command-only mapper tests

--- test/unit/presentation/discord/mappers/lfg/createCommandOnlyMessageBase.test.ts
+++ test/unit/presentation/discord/mappers/lfg/createCommandOnlyMessageBase.test.ts
@@ -3,20 +3,13 @@ import {
     createInvalidLfgSubcommandMessageBase,
     createLfgHelpMessageBase,
 } from "../../../../../../src/presentation/discord/mappers/lfg.ts";
-import { EMessageKind } from "../../../../../../src/presentation/discord/message.types.ts";
 
 describe("LFG command-only messages", () => {
     test("creates help message", () => {
-        expect(createLfgHelpMessageBase()).toMatchObject({
-            kind: EMessageKind.NEUTRAL,
-            embeds: [{ description: expect.any(String) }],
-        });
+        expect(createLfgHelpMessageBase()).toMatchSnapshot();
     });
 
     test("creates invalid subcommand message", () => {
-        expect(createInvalidLfgSubcommandMessageBase()).toMatchObject({
-            kind: EMessageKind.ERROR,
-            embeds: [{ description: "Please specify a valid subcommand." }],
-        });
+        expect(createInvalidLfgSubcommandMessageBase()).toMatchSnapshot();
     });
 });
