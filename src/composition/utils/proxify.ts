053809cda1d1617ccf7b5ded9ec1d7ddf128b4d5
NotABot_FES <notabot.fes@gmail.com>
2026-08-29 15:57:26 +0200

    Use generic build for composition

---
+++ src/composition/utils/proxify.ts
@@ -0,0 +1,56 @@
+const err = () => {
+    throw new Error(
+        `Attempted to access own keys of a record built with ${proxify.name} by providing a function but no source for keys.`,
+    );
+};
+
+const proxify = <T extends Record<PropertyKey, unknown>>(f: <K extends keyof T>(key: K) => T[K]): T => {
+    const cache = new Map<PropertyKey, unknown>();
+
+    return new Proxy(
+        {},
+        {
+            get: (_, property) => {
+                if (!cache.has(property)) {
+                    cache.set(property, f(property as keyof T));
+                }
+                return cache.get(property);
+            },
+            ownKeys: err,
+        },
+    ) as T;
+};
+
+// TODO: I'm leaving additions below now but they look all a bit too funky to me.
+// Let's try to remove as much as possible later.
+
+type TBuildableFunction = (dependencies: never, ...args: never[]) => unknown;
+type TBuildableFunctions = Record<PropertyKey, TBuildableFunction>;
+
+type TRemainingArguments<Function extends TBuildableFunction> =
+    Parameters<Function> extends [unknown, ...infer RemainingArguments] ? RemainingArguments : never;
+
+type TBuiltFunction<Function extends TBuildableFunction> = (
+    ...args: TRemainingArguments<Function>
+) => ReturnType<Function>;
+
+type TBuiltFunctions<Functions extends TBuildableFunctions> = {
+    readonly [Key in keyof Functions]: TBuiltFunction<Functions[Key]>;
+};
+
+function bindFunction<Dependencies, Function extends TBuildableFunction>(
+    dependencies: Dependencies,
+    functionToBind: Function,
+): TBuiltFunction<Function> {
+    return (...args: TRemainingArguments<Function>) =>
+        Reflect.apply(functionToBind, undefined, [dependencies, ...args]) as ReturnType<Function>;
+}
+
+export function build<Dependencies, Functions extends TBuildableFunctions>(
+    dependencies: Dependencies,
+    functions: Functions,
+): TBuiltFunctions<Functions> {
+    return proxify<TBuiltFunctions<Functions>>((key) => bindFunction(dependencies, functions[key]));
+}
+
+export default proxify;
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,18 @@
-import type { EntityManager } from "@mikro-orm/sqlite";
 import debug from "debug";
 import { Client, Events, GatewayIntentBits } from "discord.js";
-import type { TAdminPersistence } from "./application/admin/persistence.types.ts";
 import ADMIN_USE_CASES from "./application/admin/useCases.ts";
-import type { TAdminUseCaseDependencies, TAdminUseCases } from "./application/admin/useCases.types.ts";
-import type { TLfgPersistence } from "./application/lfg/persistence.types.ts";
+import { changeRoomCodeInRoom } from "./application/lfg/services/changeRoomCodeInRoom.ts";
+import { getOwnedRoom } from "./application/lfg/services/getOwnedRoom.ts";
+import { kickFromRoom } from "./application/lfg/services/kickFromRoom.ts";
+import { removePlayerFromRoom } from "./application/lfg/services/removePlayerFromRoom.ts";
+import { transferRoom } from "./application/lfg/services/transferRoom.ts";
 import LFG_USE_CASES from "./application/lfg/useCases.ts";
-import type { TLfgUseCases } from "./application/lfg/useCases.types.ts";
-import type { TGetSearchIndexEntries, TSearchPersistence } from "./application/search/persistence.types.ts";
 import { generateSearchIndexEntries } from "./application/search/searchAliases.ts";
 import SEARCH_USE_CASES from "./application/search/useCases.ts";
-import type { TSearchUseCaseDependencies, TSearchUseCases } from "./application/search/useCases.types.ts";
-import { composeLfgServices } from "./composition/application/lfg/services.ts";
-import { getWithUnitOfWork } from "./composition/application/unitOfWork.ts";
-import { getPersistenceWithContext, getUseCasesWithUnitOfWork } from "./composition/application/useCases.ts";
 import { composeDiscordCommands } from "./composition/presentation/discord/commands.ts";
 import { composeDiscordEventHandlers } from "./composition/presentation/discord/eventHandlers.ts";
+import { build } from "./composition/utils/proxify.ts";
+import type { TSearchIndexEntry } from "./domain/search/types.ts";
 import { appMikroOrmConfig } from "./infrastructure/database/mikroOrm/config.ts";
 import { initOrm } from "./infrastructure/database/mikroOrm/orm.ts";
 import ADMIN_REPOSITORIES from "./infrastructure/database/mikroOrm/repositories/admin.ts";
@@ -23,80 +20,88 @@ import LFG_REPOSITORIES from "./infrastructure/database/mikroOrm/repositories/lf
 import SEARCH_REPOSITORIES, {
     SEARCH_ALIAS_REPOSITORIES,
 } from "./infrastructure/database/mikroOrm/repositories/search.ts";
-import { FuseSearchEngine } from "./infrastructure/search/engine.ts";
+import { FuseSearchEngine, type SearchEngine } from "./infrastructure/search/engine.ts";
 
 const log = debug("index.ts");
 
 const orm = await initOrm(appMikroOrmConfig);
 const em = orm.em.fork();
 
-const withAdminUnitOfWork = getWithUnitOfWork({
-    em,
-    getDependencies: (em): TAdminUseCaseDependencies => ({
-        persistence: getPersistenceWithContext<TAdminPersistence>({
-            em,
-            repositories: ADMIN_REPOSITORIES,
-        }),
-    }),
-});
-const adminUseCases = getUseCasesWithUnitOfWork<TAdminUseCases>({
-    useCases: ADMIN_USE_CASES,
-    withUnitOfWork: withAdminUnitOfWork,
-});
-
-const withLfgUnitOfWork = getWithUnitOfWork({
-    em,
-    getDependencies: (em) => {
-        // TODO: kinda weird looking to have multiple persistences redefined here
-        const lfgPersistence = getPersistenceWithContext<Omit<TLfgPersistence, "getGuildConfig">>({
-            em,
-            repositories: LFG_REPOSITORIES,
-        });
-        const adminPersistence = getPersistenceWithContext<Pick<TAdminPersistence, "getGuildConfig">>({
-            em,
-            repositories: ADMIN_REPOSITORIES,
-        });
-        const persistence: TLfgPersistence = {
-            ...lfgPersistence,
-            getGuildConfig: adminPersistence.getGuildConfig,
-        };
-        const services = composeLfgServices(persistence);
-        return { persistence, services };
-    },
-});
-const lfgUseCases = getUseCasesWithUnitOfWork<TLfgUseCases>({
-    useCases: LFG_USE_CASES,
-    withUnitOfWork: withLfgUnitOfWork,
-});
-
 const entitiesForGeneratingSearchAliases = await SEARCH_ALIAS_REPOSITORIES.getEntitiesForGeneratingSearchAliases({
-    em: em.fork(),
+    em,
 });
 const searchItems = generateSearchIndexEntries(entitiesForGeneratingSearchAliases);
-
 const searchEngine = new FuseSearchEngine({ items: searchItems });
-const getBestSearchIndexEntry = searchEngine.searchOne.bind(searchEngine);
-const getSearchIndexEntries: TGetSearchIndexEntries = (searchArg) =>
-    searchEngine.search(searchArg.input, searchArg.limit);
-const getSearchDependencies = (em: EntityManager): TSearchUseCaseDependencies => {
-    const searchPersistence = getPersistenceWithContext<Pick<TSearchPersistence, "getEntityByKindAndId">>({
-        em,
-        repositories: SEARCH_REPOSITORIES,
-    });
-    const persistence: TSearchPersistence = {
-        getBestSearchIndexEntry,
-        getEntityByKindAndId: searchPersistence.getEntityByKindAndId,
-        getSearchIndexEntries,
-    };
-    return { persistence };
+
+// TODO: some funky business going on for this repository
+const searchRepositories = {
+    ...SEARCH_REPOSITORIES,
+    getBestSearchIndexEntry: (
+        { searchEngine }: { readonly searchEngine: SearchEngine<TSearchIndexEntry> },
+        input: string,
+    ) => searchEngine.searchOne(input),
+    getSearchIndexEntries: (
+        { searchEngine }: { readonly searchEngine: SearchEngine<TSearchIndexEntry> },
+        { input, limit }: { readonly input: string; readonly limit?: number },
+    ) => searchEngine.search(input, limit),
 };
-const withSearchUnitOfWork = getWithUnitOfWork({ em, getDependencies: getSearchDependencies });
-const searchUseCases = getUseCasesWithUnitOfWork<TSearchUseCases>({
-    useCases: SEARCH_USE_CASES,
-    withUnitOfWork: withSearchUnitOfWork,
+
+const REPOSITORIES = {
+    admin: ADMIN_REPOSITORIES,
+    // TODO: this is a sign repositories should be organized another way
+    lfg: { ...LFG_REPOSITORIES, getGuildConfig: ADMIN_REPOSITORIES.getGuildConfig },
+    search: searchRepositories,
+} as const;
+
+const APPLICATION_SERVICES = {
+    lfg: {
+        changeRoomCodeInRoom,
+        getOwnedRoom,
+        kickFromRoom,
+        removePlayerFromRoom,
+        transferRoom,
+    },
+} as const;
+
+const USE_CASES = {
+    admin: ADMIN_USE_CASES,
+    lfg: LFG_USE_CASES,
+    search: SEARCH_USE_CASES,
+} as const;
+
+const repositoriesDependencies = { em, searchEngine };
+const builtRepositories = {
+    admin: build(repositoriesDependencies, REPOSITORIES.admin),
+    lfg: build(repositoriesDependencies, REPOSITORIES.lfg),
+    search: build(repositoriesDependencies, REPOSITORIES.search),
+};
+
+const servicesDependencies = {
+    persistence: builtRepositories.lfg,
+    get services() {
+        return builtLfgServices;
+    },
+};
+const builtLfgServices = build(servicesDependencies, APPLICATION_SERVICES.lfg);
+const builtServices = { lfg: builtLfgServices };
+
+// TODO: ultimately, there should be a function that takes a record of record of useCases and builds all at once.
+// TODO: should composed types be introduced for objects like builtUseCases?
+const builtUseCases = {
+    admin: build({ persistence: builtRepositories.admin }, USE_CASES.admin),
+    lfg: build({ persistence: builtRepositories.lfg, services: builtServices.lfg }, USE_CASES.lfg),
+    search: build({ persistence: builtRepositories.search }, USE_CASES.search),
+};
+
+// TODO: ALL commands should be passed as argument to build here as well, even if they technically do not need to be built every time
+// ...which makes me wonder whether some commands should be handled specially
+// also remember that commands can have multiple layers vs fixed layers for repositories/services/use cases
+const commands = composeDiscordCommands({
+    adminUseCases: builtUseCases.admin,
+    lfgUseCases: builtUseCases.lfg,
+    searchUseCases: builtUseCases.search,
 });
-const commands = composeDiscordCommands({ adminUseCases, lfgUseCases, searchUseCases });
-const eventHandlers = composeDiscordEventHandlers({ commands, searchUseCases });
+const eventHandlers = composeDiscordEventHandlers({ commands, searchUseCases: builtUseCases.search });
 
 const intents = [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages];
 const bot = new Client({ intents });
