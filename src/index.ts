47420c6f6d82d9203af1627597179e8ec7c392d2
NotABot_FES <notabot.fes@gmail.com>
2026-08-29 22:17:27 +0200

    Move event handler composition to index

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ import { transferRoom } from "./application/lfg/services/transferRoom.ts";
 import LFG_USE_CASES from "./application/lfg/useCases.ts";
 import { generateSearchIndexEntries } from "./application/search/searchAliases.ts";
 import SEARCH_USE_CASES from "./application/search/useCases.ts";
-import { composeDiscordEventHandlers } from "./composition/presentation/discord/eventHandlers.ts";
 import { build } from "./composition/utils/proxify.ts";
 import type { TSearchIndexEntry } from "./domain/search/types.ts";
 import { appMikroOrmConfig } from "./infrastructure/database/mikroOrm/config.ts";
@@ -20,6 +19,27 @@ import SEARCH_REPOSITORIES, {
     SEARCH_ALIAS_REPOSITORIES,
 } from "./infrastructure/database/mikroOrm/repositories/search.ts";
 import { FuseSearchEngine, type SearchEngine } from "./infrastructure/search/engine.ts";
+import { AUTOCOMPLETE } from "./presentation/discord/autocomplete.ts";
+import type { TAutocompleteHandlerGetter } from "./presentation/discord/autocomplete/handlers.ts";
+import { getAutocompleteHandler as getRawAutocompleteHandlerFromHandlers } from "./presentation/discord/autocomplete/handlers.ts";
+import { COMMANDS } from "./presentation/discord/commands.ts";
+import {
+    getCommandRunHandler as getRawCommandRunHandlerFromCommands,
+    type TBuiltCommandRunHandlerGetter,
+} from "./presentation/discord/commands/handlers.ts";
+import type {
+    TBuiltCommandAutocompleteHandler,
+    TCommandAutocompleteHandler,
+} from "./presentation/discord/commands/types.ts";
+import { handleClientReady } from "./presentation/discord/eventHandlers/clientReady.ts";
+import { handleInteractionCreate } from "./presentation/discord/eventHandlers/interactionCreate.ts";
+import type { THandleInteractionCreate } from "./presentation/discord/eventHandlers/interactionCreate.types.ts";
+import { handleAutocompleteInteraction } from "./presentation/discord/eventHandlers/interactions/autocomplete.ts";
+import type { THandleAutocompleteInteraction } from "./presentation/discord/eventHandlers/interactions/autocomplete.types.ts";
+import { handleCommandInteraction } from "./presentation/discord/eventHandlers/interactions/command.ts";
+import type { THandleCommandInteraction } from "./presentation/discord/eventHandlers/interactions/command.types.ts";
+import { handleMessageCreate } from "./presentation/discord/eventHandlers/messageCreate.ts";
+import type { THandleMessageCreate } from "./presentation/discord/eventHandlers/messageCreate.types.ts";
 
 const log = debug("index.ts");
 
@@ -93,16 +113,37 @@ const builtUseCases = {
     search: build(useCasesDependencies, USE_CASES.search),
 };
 
-// TODO: the command handler should probably be built here though?
-const eventHandlers = composeDiscordEventHandlers({
-    useCases: builtUseCases,
-});
+const presentationDependencies = { useCases: builtUseCases };
+
+const messageCreate: THandleMessageCreate = (interaction) =>
+    handleMessageCreate({ interaction, resolveSearchInput: builtUseCases.search.resolveSearchInput });
+// TODO: "raw" command run handler? Confirm what it is later.
+const getRawCommandRunHandler = getRawCommandRunHandlerFromCommands(COMMANDS);
+const getCommandRunHandler: TBuiltCommandRunHandlerGetter = (interaction) => {
+    const command = getRawCommandRunHandler(interaction);
+    return command ? build(presentationDependencies, { command }).command : undefined;
+};
+const getRawAutocompleteHandler = getRawAutocompleteHandlerFromHandlers<TCommandAutocompleteHandler>(AUTOCOMPLETE);
+const getAutocompleteHandler: TAutocompleteHandlerGetter<TBuiltCommandAutocompleteHandler> = (interaction) => {
+    const autocomplete = getRawAutocompleteHandler(interaction);
+    return autocomplete ? build(presentationDependencies, { autocomplete }).autocomplete : undefined;
+};
+const commandInteraction: THandleCommandInteraction = (interaction) =>
+    handleCommandInteraction({ getCommandRunHandler, interaction });
+const autocompleteInteraction: THandleAutocompleteInteraction = (interaction) =>
+    handleAutocompleteInteraction({ getAutocompleteHandler, interaction });
+const interactionCreate: THandleInteractionCreate = (interaction) =>
+    handleInteractionCreate({
+        handleAutocompleteInteraction: autocompleteInteraction,
+        handleCommandInteraction: commandInteraction,
+        interaction,
+    });
 
 const intents = [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages];
 const bot = new Client({ intents });
-bot.on(Events.ClientReady, eventHandlers.clientReady);
-bot.on(Events.MessageCreate, eventHandlers.messageCreate);
-bot.on(Events.InteractionCreate, eventHandlers.interactionCreate);
+bot.on(Events.ClientReady, handleClientReady);
+bot.on(Events.MessageCreate, messageCreate);
+bot.on(Events.InteractionCreate, interactionCreate);
 // Implicitly use DISCORD_TOKEN
 await bot.login();
 
