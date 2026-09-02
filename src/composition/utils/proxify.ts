aee3eefeaa5cc0588b84872acb75f2fd09e1a5ce
NotABot_FES <notabot.fes@gmail.com>
2026-09-02 22:43:47 +0200

    Comment proxify

--- src/composition/utils/proxify.ts
+++ src/composition/utils/proxify.ts
@@ -7,6 +7,25 @@ const err = () => {
 };
 
 // TODO: basic proxify logic and caching should be separate functions
+/**
+ * Given a function that returns a value of a specific type for each possible {@link PropertyKey}-compatible input value,
+ * returns a proxy object typed as having keys of the same type as the input function's keys and values of the same type as the input function's return type.
+ *
+ * Example:
+ *
+ * ```ts
+ * const record = { foo: "a", bar: 1 } as const;
+ *
+ * const proxifiedRecord = proxify<typeof record>((key) => {
+ *   console.log(`Accessing key: ${key}`);
+ *   return record[key];
+ * });
+ *
+ * console.log(proxifiedRecord.foo); // Output: "a"
+ * console.log(proxifiedRecord.bar); // Output: 1
+ * console.log(proxifiedRecord.baz); // Property 'baz' does not exist on type '{ readonly foo: "a"; readonly bar: 1; }'.
+ * ```
+ */
 const proxify = <T extends Record<PropertyKey, unknown>>(f: <K extends keyof T>(key: K) => T[K]): T => {
     const cache = new Map<PropertyKey, unknown>();
 
