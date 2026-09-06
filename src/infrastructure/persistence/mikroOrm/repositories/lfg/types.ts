ca9a153164b5c4a6434b76a67490f4d078af6781
NotABot_FES <notabot.fes@gmail.com>
2026-09-06 21:53:30 +0200

    Remove TLfgRoomEntity from src/infrastructure/persistence/mikroOrm/repositories/lfg/types.ts

--- src/infrastructure/persistence/mikroOrm/repositories/lfg/types.ts
+++ src/infrastructure/persistence/mikroOrm/repositories/lfg/types.ts
@@ -1,6 +1,5 @@
 import type { EntityManager } from "@mikro-orm/sqlite";
 import type { TLfgRepository } from "../../../../../application/lfg/repositories.types.ts";
-import type { LfgRoom } from "../../models/lfg/room.ts";
 
 export type TLfgRepositoryContext = {
     readonly em: EntityManager;
@@ -10,8 +9,6 @@ export type TLfgRepositoryFunction<Function extends (...args: never[]) => unknow
     context: TLfgRepositoryContext,
     arg: Parameters<Function>[0],
 ) => ReturnType<Function>;
-
-export type TLfgRoomEntity = LfgRoom;
 
 export type TLfgRepositoryMap = {
     readonly [Key in keyof TLfgRepository]: TLfgRepositoryFunction<TLfgRepository[Key]>;
